98cf950adca3d03139ba580f6900fbce4934bc09
Zach Sommers <zsommers@kickbackpoints.com>
2016-03-24 14:35:46 -0700

    testing

--- bdo_tools/bdo_tools/wsgi.py
+++ bdo_tools/bdo_tools/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+print('~~~~~~{}.format(os.path.abspath(os.path.curdir)))
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bdo_tools.settings")
 
 application = get_wsgi_application()
