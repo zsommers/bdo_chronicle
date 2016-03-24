dd8eb8a6c8870dd7e60c89d9a804cb3710e1b8b3
Zach Sommers <zsommers@kickbackpoints.com>
2016-03-24 11:52:06 -0700

    Fixed for changed paths

--- bdo_tools/bdo_tools/settings/base.py
+++ bdo_tools/bdo_tools/settings/base.py
@@ -14,7 +14,7 @@ import os
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 def get_env_variable(var_name):
