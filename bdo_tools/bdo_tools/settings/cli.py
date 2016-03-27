a209ceae1cc41d00dd6635d51d857ac2b6796bac
Zach Sommers <zsommers@kickbackpoints.com>
2016-03-26 23:40:43 -0700

    Testing on heroku is hard

---
+++ bdo_tools/bdo_tools/settings/cli.py
@@ -0,0 +1,14 @@
+
+# Connect to a different Heroku DB for Travis testing
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': get_env_variable('BDO_DB_NAME'),
+        'USER': get_env_variable('BDO_DB_USER'),
+        'PASSWORD': get_env_variable('BDO_DB_PASSWORD'),
+        'HOST': get_env_variable('BDO_DB_HOST'),
+        'PORT': get_env_variable('BDO_DB_PORT'),
+    }
+}
+
+TEST_RUNNER = 'python.path.to.test_suite_runner.HerokuTestSuiteRunner'
