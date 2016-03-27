de58d928ba605874ca68b210954db843aa661ea7
Zach Sommers <zsommers@kickbackpoints.com>
2016-03-27 01:47:21 -0700

    Found Django option

--- bdo_tools/bdo_tools/settings/cli.py
+++ bdo_tools/bdo_tools/settings/cli.py
@@ -2,16 +2,11 @@
 from .base import *
 
 # Connect to a different Heroku DB for Travis testing
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_env_variable('BDO_DB_NAME'),
-        'USER': get_env_variable('BDO_DB_USER'),
-        'PASSWORD': get_env_variable('BDO_DB_PASSWORD'),
-        'HOST': get_env_variable('BDO_DB_HOST'),
-        'PORT': get_env_variable('BDO_DB_PORT'),
-    }
+DATABASES['default']['TEST'] = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': get_env_variable('BDO_DB_NAME'),
+    'USER': get_env_variable('BDO_DB_USER'),
+    'PASSWORD': get_env_variable('BDO_DB_PASSWORD'),
+    'HOST': get_env_variable('BDO_DB_HOST'),
+    'PORT': get_env_variable('BDO_DB_PORT'),
 }
-
-# Thanks to https://gist.github.com/gregsadetsky/5018173
-TEST_RUNNER = 'bdo_tools.heroku_test_suite_runner.HerokuTestSuiteRunner'
