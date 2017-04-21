6960832fd4a6a8539de9e9f1e642ed1f88ef1a5e
Zach Sommers <github@zachsommers.com>
2017-04-21 13:35:59 -0700

    Fix various small issues
    Rename CLI to CI because that was wrong
    Fix a number of style and import clarity issues
    Update Django Debug Toolbar version

--- bdo_tools/bdo_tools/settings/dev.py
+++ bdo_tools/bdo_tools/settings/dev.py
@@ -1,8 +1,13 @@
-from .base import *
+import os
+
+from .base import BASE_DIR, INSTALLED_APPS, TEMPLATES
+from .base import *  # NOQA
 
 DEBUG = True
 django_template_settings = [x for x in TEMPLATES if x['BACKEND'] == 'django.template.backends.django.DjangoTemplates'][0]
 django_template_settings['OPTIONS']['debug'] = True
+
+MIDDLEWARE_CLASSES += ['debug_toolbar.middleware.DebugToolbarMiddleware']
 
 INSTALLED_APPS += ['debug_toolbar']
 
--- bdo_tools/bdo_tools/settings/prod.py
+++ bdo_tools/bdo_tools/settings/prod.py
@@ -1,3 +1,6 @@
-from .base import *
+import os
+
+from .base import BASE_DIR
+from .base import *  # NOQA
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
